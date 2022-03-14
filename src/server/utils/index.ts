5fb5250a766617c53a06d828589ebd41789f1d33
masxxiii <masxafar@gmail.com>
2022-03-14 23:16:28 +0700

    feature: add error handler
    Add handler
    
    Ref: #006

--- src/server/utils/index.ts
+++ src/server/utils/index.ts
@@ -1,3 +1,28 @@
+/**
+ * Error function.
+ *
+ * @remarks
+ * Used in server handlers to define our error data.
+ *
+ * @param code - The https error code.
+ * @param msg - The https error message.
+ * @param data - The error object
+ *
+ * @returns Boom
+ */
+import {Boom} from "@hapi/boom";
+import {ERRORS} from "../errors/codes";
+
+export function error(code: number, msg: string, data: object): Boom {
+    return new Boom(msg, {
+        data: {
+            code,
+            data,
+            api: true,
+        },
+        statusCode: Math.floor(code / 1000),
+    });
+}
 
 /**
  * Handles validation error.
