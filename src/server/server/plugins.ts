c89108adf6142ab937a2697a7cf7ce11409b4d23
masxxiii <masxafar@gmail.com>
2022-03-14 23:22:22 +0700

    feature: add plugins
    Add imports and base object
    
    Ref: #007

---
+++ src/server/server/plugins.ts
@@ -0,0 +1,13 @@
+import * as Pino from 'hapi-pino';
+import * as Inert from '@hapi/inert';
+import * as Vision from '@hapi/vision';
+import * as HapiSwagger from 'hapi-swagger';
+import { ServerRegisterPluginObject, } from '@hapi/hapi';
+import * as HapiBearer from 'hapi-auth-bearer-token';
+import SwaggerOptions from '../config/swagger';
+import { pinoConfig, } from '../config/pino';
+
+
+export const plugins: Array<ServerRegisterPluginObject<any>> = [
+
+];
