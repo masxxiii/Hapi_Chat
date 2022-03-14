2519f4cb0a6e59c2565a176cac8b4a56b57fc7a6
masxxiii <masxafar@gmail.com>
2022-03-14 23:24:47 +0700

    feature: add websockets plugin
    Add websocket object
    
    Ref: #008

--- src/server/server/websocket.ts
+++ src/server/server/websocket.ts
@@ -5,4 +5,22 @@ export const subscriptions = {
     notification: '/notification',
 };
 
-type WebsocketOptions = Record<string, any>
+type WebsocketOptions = Record<string, any>;
+
+export const Websocket: Plugin<WebsocketOptions> = {
+    name: 'websocket',
+    register: async (server: Server, options: WebsocketOptions) => {
+        await server.register({
+            plugin: Nes,
+            options: {
+                ...options,
+            },
+        });
+        for (const path of Object.values(subscriptions)) {
+            await server.subscription(path);
+        }
+
+        server.method('send', (path: string, data: any) => {
+            server.publish(path, data);
+        });
+    },
